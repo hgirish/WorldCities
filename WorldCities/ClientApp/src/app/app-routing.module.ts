85064c1922af85bc30c76913a0807992f55f04c1
Girish <girish@post.com>
2021-03-16 20:42:56 -0700

    city edit

--- WorldCities/ClientApp/src/app/app-routing.module.ts
+++ WorldCities/ClientApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CitiesComponent } from './cities/cities.component';
+import { CityEditComponent } from './Cities/city-edit.component';
 import { CountriesComponent } from './countries/countries.component';
 import { HomeComponent } from './home/home.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'cities', component: CitiesComponent },
   { path: 'countries', component: CountriesComponent },
+  { path: 'city/:id', component: CityEditComponent },
 ];
 
 @NgModule({
--- WorldCities/ClientApp/src/app/cities/city-edit.component.ts
+++ WorldCities/ClientApp/src/app/cities/city-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { City } from './city';
 
 @Component({
   selector: 'app-city-edit',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-edit.component.scss']
 })
 export class CityEditComponent implements OnInit {
+  title: string;
+  form: FormGroup;
+  city: City;
 
-  constructor() { }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl:string
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      lat: new FormControl(''),
+      lon: new FormControl('')
+    });
+
+    this.loadData();
+  }
+    loadData() {
+      var id = +this.activatedRoute.snapshot.paramMap.get('id');
+
+      var url = this.baseUrl + 'api/Cities/' + id;
+      this.http.get<City>(url).subscribe(result => {
+        this.city = result;
+        this.title = "Edit - " + this.city.name;
+
+        this.form.patchValue(this.city);
+      }, error => console.error(error));
+  }
+
+  onSubmit() {
+    var city = this.city;
+
+    city.name = this.form.get("name").value;
+    city.lat = +this.form.get("lat").value;
+    city.lon = +this.form.get("lon").value;
+
+    var url = this.baseUrl + "api/Cities/" + this.city.id;
+    this.http.put<City>(url, city)
+      .subscribe(result => {
+        console.log("City " + city.id + " has been updated.");
+        this.router.navigate(['/Cities']);
+      }, error => console.error(error));
   }
 
 }
